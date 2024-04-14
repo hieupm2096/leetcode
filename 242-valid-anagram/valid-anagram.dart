9146da374c17a4195599ccbe92339cbc9c0b3c48
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-14 22:23:00 +0700

    Time: 344 ms (70.09%) | Memory: 150.1 MB (63.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.dart
+++ 242-valid-anagram/valid-anagram.dart
@@ -2,14 +2,12 @@ class Solution {
   bool isAnagram(String s, String t) {
     if (s.length != t.length) return false;
     
-    final sArr = s.split('');
-    final tArr = t.split('');
     final sMap = <String, int>{};
     final tMap = <String, int>{};
     
-    for (int i = 0; i < sArr.length; i++) {
-        sMap[sArr[i]] = (sMap[sArr[i]] ?? 0) + 1;
-        tMap[tArr[i]] = (tMap[tArr[i]] ?? 0) + 1;
+    for (int i = 0; i < s.length; i++) {
+        sMap[s[i]] = (sMap[s[i]] ?? 0) + 1;
+        tMap[t[i]] = (tMap[t[i]] ?? 0) + 1;
     }
     
     for (String key in sMap.keys) {
