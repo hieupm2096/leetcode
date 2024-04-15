09eb3f11de2455e59c1ec2de2554eb1d8294e4a9
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-16 01:40:14 +0700

    Time: 356 ms (58.37%) | Memory: 152.8 MB (74.29%) - LeetSync

---
+++ 169-majority-element/majority-element.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int majorityElement(List<int> nums) {
+    final mp = <int, int>{};
+    final mj = nums.length / 2;
+
+    for (final e in nums) {
+        mp[e] = (mp[e] ?? 0) + 1;
+        if (mp[e]! > mj) return e;
+    }
+
+    return nums[0];
+  }
+}
