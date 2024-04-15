cd03a19afa8328caa2fdb05c602301b42d3f23e2
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-16 01:56:39 +0700

    Time: 15 ms (89.25%) | Memory: 6.7 MB (48.85%) - LeetSync

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -1,11 +1,7 @@
 func majorityElement(nums []int) int {
     m := make(map[int]int)
     for i := 0; i < len(nums); i++ {
-        if _, ok := m[nums[i]]; ok {
-            m[nums[i]] += 1
-        } else {
-            m[nums[i]] = 1
-        }
+        m[nums[i]] += 1
 
         if m[nums[i]] > len(nums) / 2 {
             return nums[i]
