10005947cd29f2bc5902b111f2852aaf41e97780
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-16 01:46:53 +0700

    Time: 369 ms (43.26%) | Memory: 152.5 MB (80.82%) - LeetSync

--- 169-majority-element/majority-element.dart
+++ 169-majority-element/majority-element.dart
@@ -1,7 +1,7 @@
 class Solution {
   int majorityElement(List<int> nums) {
     final mp = <int, int>{};
-    final mj = nums.length / 2;
+    final mj = nums.length ~/ 2;
 
     for (final e in nums) {
         mp[e] = (mp[e] ?? 0) + 1;
