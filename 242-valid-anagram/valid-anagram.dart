4e87da654786266523d77c213fe56e4cff4341b6
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-18 06:36:40 +0700

    Time: 340 ms (77.98%) | Memory: 148 MB (86.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.dart
+++ 242-valid-anagram/valid-anagram.dart
@@ -1,19 +1,19 @@
 class Solution {
   bool isAnagram(String s, String t) {
     if (s.length != t.length) return false;
-    
-    final sMap = <String, int>{};
-    final tMap = <String, int>{};
-    
-    for (int i = 0; i < s.length; i++) {
-        sMap[s[i]] = (sMap[s[i]] ?? 0) + 1;
-        tMap[t[i]] = (tMap[t[i]] ?? 0) + 1;
+
+    final freqS = <String, int>{};
+    final freqT = <String, int>{};
+
+    for (var i = 0; i < s.length; i++) {
+        freqS[s[i]] = (freqS[s[i]] ?? 0) + 1;
+        freqT[t[i]] = (freqT[t[i]] ?? 0) + 1;
     }
-    
-    for (String key in sMap.keys) {
-        if (sMap[key] != (tMap[key] ?? 0)) return false;
+
+    for (final key in freqS.keys) {
+        if (freqS[key] != freqT[key]) return false;
     }
-    
+
     return true;
   }
 }
