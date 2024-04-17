44eb66032e4a2b565630124c0334ad5fb15520a2
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-18 06:50:48 +0700

    Time: 410 ms (49.12%) | Memory: 159 MB (48.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.dart
+++ 49-group-anagrams/group-anagrams.dart
@@ -1,22 +1,24 @@
 class Solution {
   List<List<String>> groupAnagrams(List<String> strs) {
     if (strs.isEmpty) return [];
+    if (strs.length == 1) return [strs];
 
     final result = <String, List<String>>{};
 
-    for (final str in strs) {
+    for (final s in strs) {
         final arr = List<int>.filled(26, 0);
 
-        for (var i = 0; i < str.length; i++) {
-            arr[str[i].codeUnitAt(0) - 'a'.codeUnitAt(0)]++;
+        for (var i = 0; i < s.length; i++) {
+        final c = s[i];
+        arr[c.codeUnitAt(0) - 'a'.codeUnitAt(0)]++;
         }
 
-        final t = arr.join('#');
+        final k = arr.join('#');
 
-        if (result.containsKey(t)) {
-            result[t]!.add(str);
+        if (result.containsKey(k)) {
+        result[k]!.add(s);
         } else {
-            result[t] = [str];
+        result[k] = [s];
         }
     }
 
