6c56a9802ab1aa1d99ef577540ad7ba71ad7e4cc
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-18 01:44:29 +0700

    Time: 398 ms (22.22%) | Memory: 160.7 MB (100.00%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.dart
@@ -0,0 +1,27 @@
+class Solution {
+  int longestOnes(List<int> nums, int k) {
+    if (nums.length < 2) return nums.length;
+
+    var longest1 = 0, l = 0, r = 0, count = 0;
+
+    while (r < nums.length) {
+        var e = nums[r];
+
+        if (e == 0) count++;
+
+        if (count > k) {
+            var e1 = nums[l];
+            if (e1 == 0) count--;
+            l++;
+        } else {
+            if (longest1 < r - l + 1) {
+                longest1 = r - l + 1;
+            }
+        }
+
+        r++;
+    }
+
+    return longest1;
+  }
+}
