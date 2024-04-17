954428eb51398a4da610a53defa27ccf660b2d1c
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-17 16:32:56 +0700

    Time: 370 ms (86.59%) | Memory: 155.8 MB (5.36%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.dart
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.dart
@@ -1,36 +1,36 @@
 class Solution {
   int lengthOfLongestSubstring(String s) {
-    if (s.length == 0) return 0;
+    if (s.length < 2) return s.length;
 
     var l = 0, r = 0;
-    final result = <String, int>{};
-    var count = 0;
-    var longest = 0;
+    final freq = <String, int>{};
+    var counter = 0;
+    var result = 0;
 
     while (r < s.length) {
         final c = s[r];
+        freq[c] = (freq[c] ?? 0) + 1;
 
-        result[c] = (result[c] ?? 0) + 1;
+        if (freq[c]! == 1) {
+            counter++;
+            if (counter > result) result = counter;
+        }
 
-        if (result[c]! == 1) {
-            count++;
-            if (count > longest) longest = count;
-        } else {
-            while (result[c]! > 1) {
-                final c2 = s[l];
+        while (freq[c]! > 1) {
+            final c2 = s[l];
 
-                if (result.containsKey(c2)) {
-                    result[c2] = result[c2]! - 1;
-                    if (result[c2]! == 0) count--;
-                }
+            if (freq.containsKey(c2)) {
+                freq[c2] = freq[c2]! - 1;
 
-                l++;
+                if (freq[c2]! == 0) counter--;
             }
+            
+            l++;
         }
 
         r++;
     }
 
-    return longest;
+    return result;
   }
 }
