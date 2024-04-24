72b2108b61a645fd9c55e0af72354daf21aaf8a3
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-24 17:10:46 +0700

    Time: 357 ms (55.14%) | Memory: 152.2 MB (15.68%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.dart
+++ 2058-concatenation-of-array/concatenation-of-array.dart
@@ -1,12 +1,5 @@
 class Solution {
   List<int> getConcatenation(List<int> nums) {
-    final answer = List<int>.filled(nums.length * 2, 0);
-
-    for (var i = 0; i < nums.length; i++) {
-        answer[i] = nums[i];
-        answer[i + nums.length] = nums[i];
-    }
-
-    return answer;
+    return nums + nums;
   }
 }
