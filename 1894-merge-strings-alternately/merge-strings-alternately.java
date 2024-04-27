6e3cfdb1f639a7ca95a4d3581c47bc28897badac
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-27 16:53:20 +0700

    Time: 1 ms (85.31%) | Memory: 41.3 MB (88.23%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -3,19 +3,19 @@ class Solution {
         if (word1.length() == 0) return word2;
         if (word2.length() == 0) return word1;
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int n = word1.length() > word2.length() ? word1.length() : word2.length();
 
         for (int i = 0; i < n; i++) {
             if (i < word1.length()) {
-                result += word1.charAt(i);
+                result.append(word1.charAt(i));
             }
             
             if (i < word2.length()) {
-                result += word2.charAt(i);
+                result.append(word2.charAt(i));
             }
         }
 
-        return result;
+        return result.toString();
     }
 }
