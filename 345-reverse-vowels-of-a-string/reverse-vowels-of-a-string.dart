64fb4f1595db6e1a030228072fb37db5900d8dac
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-28 22:10:39 +0700

    Time: 384 ms (48.15%) | Memory: 152 MB (48.15%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.dart
@@ -0,0 +1,45 @@
+class Solution {
+  String reverseVowels(String s) {
+    var l = 0, r = s.length - 1;
+    final lq = <int, String>{}, rq = <int, String>{};
+
+    final arr = s.split('');
+
+    while (l < r && r > 0 && l < s.length) {
+        if (arr[l].isVowel()) {
+        lq[l] = arr[l];
+        }
+
+        if (arr[r].isVowel()) {
+        rq[r] = arr[r];
+        }
+
+        if (lq.isNotEmpty && rq.isNotEmpty) {
+        final lIndex = lq.keys.first;
+        final rIndex = rq.keys.first;
+
+        arr[lIndex] = rq[rIndex]!;
+        arr[rIndex] = lq[lIndex]!;
+
+        lq.remove(lIndex);
+        rq.remove(rIndex);
+
+        l++;
+        r--;
+        } else if (lq.isEmpty) {
+        l++;
+        } else if (rq.isEmpty) {
+        r--;
+        }
+    }
+
+    return arr.join('');
+  }
+}
+
+extension StringExt on String {
+    bool isVowel() {
+        return this == 'a' || this == 'e' || this == 'i' || this == 'o' || this == 'u'
+         || this == 'A' || this == 'E' || this == 'I' || this == 'O' || this == 'U';
+    }
+}
