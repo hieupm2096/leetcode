76f815c30bede7a3ad6d0da144e017acbaae0222
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-04-29 17:00:48 +0700

    Time: 406 ms (25.89%) | Memory: 164.9 MB (30.46%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.dart
+++ 238-product-of-array-except-self/product-of-array-except-self.dart
@@ -1,21 +1,20 @@
 class Solution {
   List<int> productExceptSelf(List<int> nums) {
-    if (nums.isEmpty) return [];
-
-    final answer = List.filled(nums.length, 1);
-
     var curr = 1;
+    final result = List.filled(nums.length, 1);
+
     for (var i = 0; i < nums.length; i++) {
-        answer[i] *= curr;
+        result[i] *= curr;
         curr *= nums[i];
     }
 
     curr = 1;
+
     for (var i = nums.length - 1; i >= 0; i--) {
-        answer[i] *= curr;
+        result[i] *= curr;
         curr *= nums[i];
     }
-    
-    return answer;
+
+    return result;
   }
 }
