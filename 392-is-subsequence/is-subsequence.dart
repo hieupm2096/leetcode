c1872a3fb3d8bf59c7d8cec21bea5bc856c9b7ba
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-05-01 20:10:53 +0700

    Time: 304 ms (77.57%) | Memory: 146.3 MB (95.33%) - LeetSync

--- 392-is-subsequence/is-subsequence.dart
+++ 392-is-subsequence/is-subsequence.dart
@@ -1,16 +1,13 @@
 class Solution {
   bool isSubsequence(String s, String t) {
     var i = 0, j = 0;
-    var result = List<bool>.filled(s.length, false);
-    while (i < s.length && j < t.length) {
+    while (j < t.length && i < s.length) {
         if (s[i] == t[j]) {
-            result[i] = true;
             i++;
         }
         j++;
     }
     
-    return result.every((e) => e);
-
+    return i >= s.length;
   }
 }
