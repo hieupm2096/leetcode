5207aadbfd34284027f636c4f9bed6a06be4838e
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-05-02 21:14:39 +0700

    Time: 345 ms (91.67%) | Memory: 165 MB (8.33%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.dart
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.dart
@@ -7,24 +7,17 @@ class Solution {
     var max = 0;
 
     while (r < nums.length) {
-        if (nums[r] == 0) {
-            count--;
-        }
+        if (nums[r] == 0) count--;
 
-        if (count == 0 || count > 0 && r == nums.length - 1) {
-            if (r - l + 1 > max) max = r - l + 1;
-        }
-
-        while (count < 0 && l < nums.length) {
-            if (nums[l] == 0) {
-                count++;
-            }
+        while (count < 0) {
+            if (nums[l] == 0) count++;
             l++;
         }
 
+        if (r - l + 1 > max) max = r - l + 1;
+
         r++;
     }
-
 
     return max;
   }
