edc654529cfa75d43fedcaf37de6de0662140c7d
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-05-02 16:48:29 +0700

    Time: 399 ms (60.61%) | Memory: 174.3 MB (36.36%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.dart
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.dart
@@ -3,22 +3,20 @@ import 'dart:math';
 class Solution {
   double findMaxAverage(List<int> nums, int k) {
     var l = 0, r = 0;
-    var max = pow(10, 4) * -1 * nums.length;
-    var count = 0, sum = 0;
+    int? max;
+    var sum = 0;
     while (r < nums.length) {
         sum += nums[r];
 
-        if (count == k - 1) {
-            count--;
-            if (sum > max) max = sum;
+        if (r - l + 1 == k) {
+            if (max == null || sum > max!) max = sum;
             sum -= nums[l];
             l++;
         }
 
         r++;
-        count++;
     }
 
-    return max / k;
+    return max! / k;
   }
 }
