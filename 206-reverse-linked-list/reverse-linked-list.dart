9f3603c9a70aa82807fa9536fc92cd35895775df
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-05-06 02:32:28 +0700

    Time: 372 ms (6.44%) | Memory: 150.2 MB (46.04%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.dart
+++ 206-reverse-linked-list/reverse-linked-list.dart
@@ -7,19 +7,34 @@
  * }
  */
 class Solution {
-  ListNode? reverseList(ListNode? head) {
-    if (head == null) return null;
-    ListNode? curr;
-    ListNode? temp;
+//   ListNode? reverseList(ListNode? head) {
+//     if (head == null) return null;
+//     ListNode? curr;
+//     ListNode? temp;
     
-    // iteractively
-    while (head != null) {
-        temp = head?.next;
-        head?.next = curr;
-        curr = head;
-        head = temp;
+//     // iteractively
+//     while (head != null) {
+//         temp = head?.next;
+//         head?.next = curr;
+//         curr = head;
+//         head = temp;
+//     }
+
+//     return curr;
+//   }
+    ListNode? reverseList(ListNode? head) {
+        if (head == null || head.next == null) return head;
+        
+        return reverse(null, head);
     }
 
-    return curr;
-  }
+    ListNode? reverse(ListNode? prev, ListNode? curr) {
+        if (curr == null) return prev;
+
+        final next = curr.next;
+
+        curr.next = prev;
+
+        return reverse(curr, next);
+    }
 }
