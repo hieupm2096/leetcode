29b2f306b7af8e126685c79ab993463c9f2d35a7
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-05-12 17:45:33 +0700

    Time: 720 ms (6.67%) | Memory: 241.8 MB (6.67%) - LeetSync

--- 146-lru-cache/lru-cache.dart
+++ 146-lru-cache/lru-cache.dart
@@ -1,36 +1,33 @@
 class LRUCache {
-    late Map<int, int> _cache;
-    late int _capacity;
+  late final int _capacity;
+  late final Map<int, int> _cache;
 
-    LRUCache(int capacity) {
-        _cache = {};
-        _capacity = capacity;
-    }
+  LRUCache(int capacity) {
+    _capacity = capacity;
+    _cache = {};
+  }
   
-    int get(int key) {
-        if (!_cache.containsKey(key)) return -1;
-        final value = _cache[key]!;
+  int get(int key) {
+    if (!_cache.containsKey(key)) return -1;
 
-        // to make sure the key is at the last of map -> most recently used
-        _cache.remove(key);
-        _cache[key] = value;
+    final value = _cache[key]!;
 
-        return value;
-    }
+    _cache.remove(key);
+    _cache[key] = value;
+
+    return value;
+  }
   
-    void put(int key, int value) {
-        if (_cache.containsKey(key)) {
-            _cache.remove(key);
-        } else if (_cache.length >= _capacity) {
-            final lruKey = _cache.keys.first;
-            _cache.remove(lruKey);
-        }
-
-        _cache[key] = value;
+  void put(int key, int value) {
+    if (_cache.length == _capacity && !_cache.containsKey(key)) {
+      final lruKey = _cache.keys.first;
+      _cache.remove(lruKey);
     }
+
+    _cache.remove(key);
+    _cache[key] = value;
+  }
 }
-
-
 
 /**
  * Your LRUCache object will be instantiated and called as such:
