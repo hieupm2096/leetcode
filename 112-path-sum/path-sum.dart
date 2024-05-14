01954d93bd94aac3764b2e4547639cc6927672d4
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-05-14 23:45:22 +0700

    Time: 349 ms (56.76%) | Memory: 147.9 MB (86.49%) - LeetSync

---
+++ 112-path-sum/path-sum.dart
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  bool hasPathSum(TreeNode? root, int targetSum) {
+    if (root == null) return false;
+
+    return _dfs(root, 0, targetSum);
+  }
+
+  bool _dfs(TreeNode? node, int currentSum, int targetSum) {
+    if (node == null) return false;
+
+    final newSum = currentSum + node.val;
+
+    if (node.left == null && node.right == null) return newSum == targetSum;
+
+    final r1 = _dfs(node.left, newSum, targetSum);
+
+    if (r1) return r1;
+
+    final r2 = _dfs(node.right, newSum, targetSum);
+
+    if (r2) return r2;
+
+    return false;
+  }
+}
