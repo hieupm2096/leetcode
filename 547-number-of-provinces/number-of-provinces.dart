6785c3134c5c79d5e7d765177aa7ff244f018147
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-05-21 01:50:28 +0700

    Time: 402 ms (14.29%) | Memory: 153.3 MB (57.14%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.dart
+++ 547-number-of-provinces/number-of-provinces.dart
@@ -1,32 +1,32 @@
-import 'dart:collection' show Queue, HashSet;
+import 'dart:collection' show HashSet;
 
 class Solution {
   int findCircleNum(List<List<int>> isConnected) {
-    final queue = Queue<List<int>>();
-    final visited = HashSet<int>();
+    final visited = <int>{};
+
     var res = 0;
 
     for (var i = 0; i < isConnected.length; i++) {
       if (visited.contains(i)) continue;
 
-      visited.add(i);
-      queue.addLast(isConnected[i]);
-
-      while (queue.isNotEmpty) {
-        final neighbors = queue.removeFirst();
-
-        for (var i = 0; i < neighbors.length; i++) {
-          if (visited.contains(i)) continue;
-          if (neighbors[i] == 1) {
-            queue.addLast(isConnected[i]);
-            visited.add(i);
-          }
-        }
-      }
+      _dfs(isConnected, visited, i);
 
       res++;
     }
 
     return res;
+  }
+
+  void _dfs(List<List<int>> isConnected, Set<int> visited, int currentNode) {
+    final node = isConnected[currentNode];
+
+    for (var i = 0; i < node.length; i++) {
+      if (currentNode == i || visited.contains(i)) continue;
+
+      if (node[i] == 1) {
+        visited.add(i);
+        _dfs(isConnected, visited, i);
+      }
+    }
   }
 }
