0bd4bb23f110950949191deb757d61f255b5a427
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-06-11 14:42:15 +0700

    Time: 339 ms (85.71%) | Memory: 147.1 MB (100.00%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.dart
+++ 1217-relative-sort-array/relative-sort-array.dart
@@ -6,10 +6,14 @@ class Solution {
 
     final sub = <int>[];
 
+    // n = arr1.length, m = arr2.length
+
+    // O(m)
     for (final e in arr2) {
       freq[e] = <int>[];
     }
 
+    // O(n)
     for (final e in arr1) {
       if (freq.containsKey(e)) {
         freq[e]!.add(e);
@@ -18,12 +22,17 @@ class Solution {
       }
     }
 
+    // O(m)
     final res = freq.values.expand((e) => e).toList();
 
+    // O((n - m)log(n - m))
     sub.sort();
 
+    // O(n - m)
     res.addAll(sub);
 
+    // to sum up: O(m + n + m + (n-m) + (n-m)log(n- m))
+    // = O(m + n + (n - m)log(n - m))
     return res;
   }
 }
