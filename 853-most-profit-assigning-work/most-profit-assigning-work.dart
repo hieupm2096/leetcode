7287a118b678f24a27991e0449826f3f08382f6a
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-06-18 17:56:45 +0700

    Time: 471 ms (100.00%) | Memory: 156.9 MB (100.00%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.dart
@@ -0,0 +1,41 @@
+import 'dart:math' show max;
+
+class Solution {
+  int maxProfitAssignment(List<int> difficulty, List<int> profit, List<int> worker) {
+    final dp = <(int, int)>[];
+
+    var n = difficulty.length;
+
+    worker.sort();
+
+    for (var i = 0; i < n; i++) {
+      if (difficulty[i] > worker.last) continue;
+
+      dp.add((difficulty[i], profit[i]));
+    }
+
+    if (dp.isEmpty) return 0;
+
+    dp.sort((a, b) => a.$1.compareTo(b.$1));
+
+    var index = 0, ans = 0, maxProfit = 0;
+
+    for (final ability in worker) {
+      while (index < dp.length && ability >= dp[index].$1) {
+        maxProfit = max(maxProfit, dp[index].$2);
+        index++;
+      }
+
+      ans += maxProfit;
+    }
+
+    return ans;
+  }
+}
+
+// n jobs, m workers
+// difficulty, profit
+// job with difficulty <= worker
+// highest profit
+
+// PriorityQueue sorts difficulty -> 
