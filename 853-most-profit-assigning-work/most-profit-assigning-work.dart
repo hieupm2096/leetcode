8176fa5fe91637ffe5ecaf8869fccff1549f02d2
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-06-18 17:58:41 +0700

    Time: 582 ms (100.00%) | Memory: 156.6 MB (100.00%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.dart
+++ 853-most-profit-assigning-work/most-profit-assigning-work.dart
@@ -18,9 +18,6 @@ class Solution {
 
     dp.sort((a, b) => a.$1.compareTo(b.$1));
 
-    print(dp);
-    print(worker);
-
     var index = 0, ans = 0, maxProfit = 0;
 
     for (final ability in worker) {
