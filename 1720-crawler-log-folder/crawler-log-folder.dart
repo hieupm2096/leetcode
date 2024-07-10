a35ffd360ace53786b5accebef5b0dcb1964d238
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-07-10 16:34:04 +0700

    Time: 324 ms (100.00%) | Memory: 148.9 MB (100.00%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.dart
+++ 1720-crawler-log-folder/crawler-log-folder.dart
@@ -3,23 +3,25 @@
 // process: keep track of number of added child folder
 // using a stack
 // [] -> [d1] -> [d1, d2] -> [d1] -> [d1, d2] -> [d1, d2]
+// to go back to main folder -> pop the op from stack
+// the min number of ops needed to go back to main folder = stack.length
 
 class Solution {
   int minOperations(List<String> logs) {
-    final stack = <String>[];
+    var count = 0;
 
     for (final op in logs) {
       if (op == './') {
         // do nothing
       } else if (op == '../') {
-        if (stack.isNotEmpty) {
-          stack.removeLast();
+        if (count != 0) {
+          count--;
         }
       } else {
-        stack.add(op);
+        count++;
       }
     }
 
-    return stack.length;
+    return count;
   }
 }
