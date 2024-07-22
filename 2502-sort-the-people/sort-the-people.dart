7bbb4645523eb0e5388088447e2b3be08ce0608b
Hieu Pham <33806842+hieupm2096@users.noreply.github.com>
2024-07-22 19:25:02 +0700

    Time: 393 ms (37.14%) | Memory: 156.4 MB (14.29%) - LeetSync

--- 2502-sort-the-people/sort-the-people.dart
+++ 2502-sort-the-people/sort-the-people.dart
@@ -1,15 +1,9 @@
-import 'dart:collection' show HashMap;
-
 class Solution {
   List<String> sortPeople(List<String> names, List<int> heights) {
-    final mp = HashMap<int, String>();
-    
-    for (var i = 0; i < heights.length; i++) {
-      mp[heights[i]] = names[i];
-    }
+    final sortedIndices = List<int>.generate(heights.length, (i) => i);
 
-    heights.sort((a, b) => b.compareTo(a));
+    sortedIndices.sort((a, b) => heights[b].compareTo(heights[a]));
 
-    return heights.map((e) => mp[e]!).toList();
+    return sortedIndices.map((e) => names[e]).toList();
   }
 }
